3a174a3bccb63da16a703a1759fe512b5342320f
Steve Bink <steve@binkcms.com>
2017-05-28 15:37:26 -0400

    Added assets directory with signature file and interstate font

---
+++ assets/open-sans-html-head-link.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sbink
+ * Date: 5/6/2017
+ * Time: 6:35 PM
+ */
+
+echo '<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300i,700|Open+Sans:300,300i,400,400i,700,700i" rel="stylesheet">';
