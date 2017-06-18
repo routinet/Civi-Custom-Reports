b0dd65b41d51b2a67b9cbd9f66f55437f521924e
Steve Bink <steve@binkcms.com>
2017-06-18 06:22:11 -0400

    Add replacement card report

---
+++ CRM/Customreports/Form/Task/MembershipReplacement.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Write a custom report to a letter (PDF).
+ */
+class CRM_Customreports_Form_Task_MembershipReplacement extends CRM_Customreports_Form_Task_MembershipBase {
+  protected $templateName = 'MembershipReplacement';
+  protected $templateTitle = 'Membership - Replacement Card';
+  protected $context = 'membership';
+
+}
