847ecd5aa6b407765a26df832952bc8441416be3
Steve Bink <steve@binkcms.com>
2017-07-12 01:04:12 -0400

    Restore the digital signature variable, remove it in letter customization

--- CRM/Customreports/Form/Task/ContributionLetterPledge.php
+++ CRM/Customreports/Form/Task/ContributionLetterPledge.php
@@ -6,4 +6,19 @@ class CRM_Customreports_Form_Task_ContributionLetterPledge extends CRM_Customrep
   protected $templateName = 'ContributionLetterPledge';
   protected $templateTitle = 'Contribution Letter - Pledge';
   protected $reportName = 'PledgeContributionDetail';
+
+  /**
+   * Remove the electronic signature file.
+   *
+   * @param $smarty The smarty template object
+   * @param $row The data row being rendered
+   */
+  public function assignExtraTokens(&$smarty, $row) {
+    // Make sure the digital signature does not print.  We call renderSignature() here
+    // to make sure a) no extra white-space is necessary in the template file, and
+    // b) the same, standardized HTML is used for this space.
+    $amount_array = ['net_amount' => CRM_Customreports_Helper::$signatureMinimumAmount];
+    $append_array = ['electronic_signature' => CRM_Customreports_Helper::renderSignature($amount_array)];
+    $smarty->append($this->context, $append_array, TRUE);
+  }
 }
